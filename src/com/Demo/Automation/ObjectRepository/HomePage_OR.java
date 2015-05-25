e8eaeb48e35c8248482a759cfe2a3352d711431e
ashish.jain@equinox.com <ashish.jain@NDI-LAP-399.3pillar.corp>
2015-05-25 03:19:28 -0400

    android demo

---
+++ src/com/Demo/Automation/ObjectRepository/HomePage_OR.java
@@ -0,0 +1,5 @@
+package com.Demo.Automation.ObjectRepository;
+
+public class HomePage_OR {
+
+}
---
+++ src/com/Demo/Automation/Pages/BasePage.java
@@ -0,0 +1,7 @@
+package com.Demo.Automation.Pages;
+
+import com.Demo.Automation.ObjectRepository.HomePage_OR;
+
+public class BasePage extends HomePage_OR{
+
+}
