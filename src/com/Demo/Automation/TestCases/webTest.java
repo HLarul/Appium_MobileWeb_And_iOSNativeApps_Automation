6968644a5b7fd4a99348d933601e79c5c810aa92
Amit Rawat <amit.rawat@equinox.com>
2015-06-17 18:21:22 +0530

    cleaned up the code

--- src/com/Demo/Automation/TestCases/webTest.java
+++ src/com/Demo/Automation/TestCases/webTest.java
@@ -14,13 +14,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
@@ -30,182 +32,112 @@ import org.testng.annotations.Test;
 
 public class webTest{
 
-WebDriver wd;
+WebDriver ad;
 
 @AfterClass
 public void afterClass()
 {
-	wd.quit();
+	ad.quit();
 }
 
 @Test(priority=1)
-public void createDriverForAndroid_Device_Web_WithAppium() throws MalformedURLException
-{
+public void AppiumAndroidChromeTest() throws MalformedURLException, InterruptedException {
 	DesiredCapabilities capabilities = new DesiredCapabilities();
-	ChromeOptions options=new ChromeOptions();
-	options.setExperimentalOption("androidPackage", "com.android.chrome");
-	capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-    capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-    capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"YT910QEQ6K");
-    capabilities.setCapability(MobileCapabilityType.VERSION, "5.0");
+    capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, MobileBrowserType.CHROME);
+//    capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"4d00408661cda0b5");
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.4.2");
+	capabilities.setCapability(MobileCapabilityType.LAUNCH_TIMEOUT,"300000");
+	capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,300);
     try 
     {
-        wd = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        wd.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-        wd.get("http://google.com");
-        try {
-			Thread.sleep(Long.parseLong("10"));
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		ad = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		ad.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		ad.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		ad.get("http://google.com");
+		ad.findElement(By.name("q")).sendKeys("Appium Meetup Noida");
+		ad.findElement(By.xpath("//button[@type='submit']")).click();
+//		ad.findElement(By.xpath("//article/section[1]/div/a")).click();
+		ad.findElement(By.xpath("//li[1]/div/h3/a")).click();
+		String meetupTitle = ad.findElement(By.xpath("//div[@class='doc-content ']/h1")).getText();
+		Assert.assertEquals(meetupTitle,"Appium: Mobile Automation Made Awesome");
+
+
+		String Screenshotpath = System.getProperty("user.dir") +  "/Screenshots/";
+		File scrFile = ((TakesScreenshot)ad).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrFile, new File(Screenshotpath + "chrome_ss.png"));
+
+	}
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+	finally{
+		ad.close();
+	}
+ }
+
+	@Test(priority=1)
+	public void AppiumAndroidNativeBrowserTest() throws MalformedURLException, InterruptedException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, MobileBrowserType.BROWSER);
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Android Emulator");
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.0.1");
+		capabilities.setCapability(MobileCapabilityType.LAUNCH_TIMEOUT,"300000");
+		capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,300);
+		try
+		{
+			ad = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			ad.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			ad.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			ad.get("http://google.com");
+			ad.findElement(By.name("q")).sendKeys("Appium Meetup Noida");
+			ad.findElement(By.xpath("//button[@type='submit']")).click();
+//		ad.findElement(By.xpath("//article/section[1]/div/a")).click();
+			ad.findElement(By.xpath("//li[1]/div/h3/a")).click();
+			String meetupTitle = ad.findElement(By.xpath("//div[@class='doc-content ']/h1")).getText();
+			Assert.assertEquals(meetupTitle,"Appium: Mobile Automation Made Awesome");
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
-        wd.close();
-    }
-    catch (MalformedURLException e) 
-    {
-        System.out.println("URL init error");
-    }
- }
-	
-	@Test(priority=2)
-	public void createDriverForAndroid_Device_Web_WithoutAppium()
-	{
-		File file=new File("D:/AutomationMeetUp/Android_Web_Automation/Tool");
-		File file1=new File(file, "chromedriver.exe");
-		DesiredCapabilities cap=new DesiredCapabilities();
-		ChromeOptions options=new ChromeOptions();
-		options.setExperimentalOption("androidPackage", "com.android.chrome");
-		cap.setCapability(ChromeOptions.CAPABILITY, options);
-		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-		cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME,"YT910QEQ6K");
-		cap.setCapability(MobileCapabilityType.VERSION, "5.0");
-		System.setProperty("webdriver.chrome.driver", file1.getAbsolutePath());
-		wd = new ChromeDriver(cap);
-		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		wd.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-		wd.get("http://yahoo.com");
-	    try {
-				Thread.sleep(Long.parseLong("10"));
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	        wd.close();
-
-}
+		finally{
+			ad.close();
+		}
+	}
 
 	@Test(priority=3)
-	public void createDriverForAndroid_Emulator_nativeBrowser() throws MalformedURLException
+	public void AndroidChromeTestUsingChromeDriver()
 	{
-		DesiredCapabilities capabilities = new DesiredCapabilities();        
-        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, MobileBrowserType.BROWSER);        
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");        
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");        
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.1");        
-        wd = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        wd.get("http://www.yahoo.com");
-	 }
-	
-	@Test(priority=4)
-	public void createDriverForAndroid_Device_nativeBrowser() throws MalformedURLException
-	{
-		File file=new File("D:/DPLAT_Automation/dplat/Tool");
-		File file1=new File(file, "chromedriver.exe");
-		DesiredCapabilities capabilities = new DesiredCapabilities();        
+		String ChromeBinaryPath = System.getProperty("user.dir") +  "/Tool/chromedriver";
+		File file=new File(ChromeBinaryPath);
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
 		ChromeOptions options=new ChromeOptions();
 		options.setExperimentalOption("androidPackage", "com.android.chrome");
 		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, MobileBrowserType.BROWSER);        
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");        
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "4df1ad83692c7fef");        
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.3");        
-        System.setProperty("webdriver.chrome.driver", file1.getAbsolutePath());
-		wd = new ChromeDriver(capabilities);
-		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		wd.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-		wd.get("http://yahoo.com");
-	    try {
-				Thread.sleep(Long.parseLong("10"));
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	        wd.close();
-	 }
 
-	
-	@Test(priority=5)
-	public void DownloadFile_Device_Chrome() throws MalformedURLException
-	{
-		File file=new File("D:/AutomationMeetUp/Android_Web_Automation/Tool");
-		File file1=new File(file, "chromedriver.exe");
-		String downloadFilepath="D:/AutomationMeetUp/Android_Web_Automation/Tool";
-		DesiredCapabilities cap=new DesiredCapabilities();
-		ChromeOptions options=new ChromeOptions();
-		options.setExperimentalOption("androidPackage", "com.android.chrome");
-		cap.setCapability(ChromeOptions.CAPABILITY, options);
-		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-		cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME,"YT910QEQ6K");
-		cap.setCapability(MobileCapabilityType.VERSION, "5.0");
-		Map<String, Object> prefs = new HashMap<String, Object>();
-		prefs.put("profile.default_content_settings.popups", 0);
-		prefs.put("download.default_directory", downloadFilepath);
-		prefs.put("savefile.default_directory", downloadFilepath);
-		cap.setCapability(ChromeOptions.CAPABILITY, prefs);
-		System.setProperty("webdriver.chrome.driver", file1.getAbsolutePath());
-		wd = new ChromeDriver(cap);
-		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		wd.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-		wd.get("http://filehippo.com/download_ted_notepad/download/b5170d0cbfa092908cb8664513f5ed6b");
-	    try {
-				Thread.sleep(Long.parseLong("10"));
-				 wd.findElement(By.xpath("//a[contains(@href,'/download/file/')]")).click();
-				 Thread.sleep(Long.parseLong("10"));
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		 wd.close();
-	 }
-	
-	
-	@Test(priority=6)
-	public void DownloadFile_Emulator_NativeBrowser() throws MalformedURLException
-	{
-		DesiredCapabilities capabilities = new DesiredCapabilities();        
-        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, MobileBrowserType.BROWSER);        
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");        
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");        
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.1");        
-        wd = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        wd.get("http://filehippo.com/download_ted_notepad/download/b5170d0cbfa092908cb8664513f5ed6b");
-        try {
-			Thread.sleep(Long.parseLong("10"));
-			 wd.findElement(By.xpath("//a[contains(@href,'/download/file/')]")).click();
-			 Thread.sleep(Long.parseLong("10"));
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		try
+		{
+			ad = new ChromeDriver(capabilities);
+			ad.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			ad.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			ad.get("http://google.com");
+			ad.findElement(By.name("q")).sendKeys("Appium Meetup Noida");
+			ad.findElement(By.xpath("//button[@type='submit']")).click();
+//		ad.findElement(By.xpath("//article/section[1]/div/a")).click();
+			ad.findElement(By.xpath("//li[1]/div/h3/a")).click();
+			String meetupTitle = ad.findElement(By.xpath("//div[@class='doc-content ']/h1")).getText();
+			Assert.assertEquals(meetupTitle,"Appium: Mobile Automation Made Awesome");
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
-        wd.close();
-	}
+		finally{
+			ad.close();
+		}
+}
 }
